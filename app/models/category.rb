f33325b5bafca6ed5ceef952db4b3dd5a4558997
Admas <molla.admas@gmail.com>
2012-07-19 16:03:00 -0500

    modified the projects concept, plus a bit more tinkering
    I finally got the projects to successfully show the list of categories
    the way i wanted it.
    
    Problems:
      I added seeds to the members db with no luck
      session stuff is slightly confusing, I'll need to read up more on it

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   attr_accessible :title
   validates :title, uniqueness: true
+  has_many :projects
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
   attr_accessible :title, :total_investment_needed, :due_date, :return_date, :percent_return, :member_id, :category_id
   validates :title, uniqueness: true
+  validates_presence_of :title, :total_investment_needed, :due_date, :return_date, :percent_return, :category_id
+	belongs_to :category
+  belongs_to :member
 end
