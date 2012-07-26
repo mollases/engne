addf1cdfdf50492b3ecfd8d7180320548617f55e
Admas <molla.admas@gmail.com>
2012-07-26 18:53:14 -0500

    Index page works!

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Engne::Application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "members#new", :as => "sign_up"
+  get "index" => "home#index"
   root :to => "home#index"
   
   resources :sessions
