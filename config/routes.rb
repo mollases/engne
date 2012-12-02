67e0c3a55b04bb66b610d1869f3e856f39b45d05
Krishna Halaharvi <halaharvi.krishna@gmail.com>
2012-12-02 14:50:36 -0600

    deleted scratch

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Engne::Application.routes.draw do
-  get "scratch_test/home"
-
-  get "scratch_test/index"
 
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "members#new", :as => "sign_up"
   get "index" => "home#index"
-  get "test" => "scratch_test#home"
-  get "test_one" => "scratch_test#index"
+  
 
   root :to => "home#index"
   
