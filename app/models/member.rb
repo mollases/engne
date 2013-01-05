a8d6b98a866c4c21aa4ba3ef6b8840e5107a67ef
Admas <molla.admas@gmail.com>
2013-01-05 16:32:43 -0600

    finished setting up devise for Member

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,27 +1,11 @@
 class Member < ActiveRecord::Base
-  attr_accessor :password
-  before_save :encrypt_password
-  has_many :projects, dependent: :destroy
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
-  validates_confirmation_of :password, :on => :create, :message => "Can't be blank"
-	#validates_confirmation_of :password
-	validates_presence_of :email, :first_name, :last_name, :password
-	validates_uniqueness_of :email
-  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name
-
-  def self.authenticate(email,password)
-    member = find_by_email(email)
-    if member && member.password_digest == BCrypt::Engine.hash_secret(password,member.password_salt)
-      member
-    else
-      nil
-    end
-  end
-  def encrypt_password
-  	if password.present?
-  		self.password_salt = BCrypt::Engine.generate_salt
-  		self.password_digest = BCrypt::Engine.hash_secret(password,password_salt)
-  	end
-  end
-
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
 end
