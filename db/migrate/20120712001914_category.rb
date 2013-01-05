8b196701af1c46dbbca96a23d1abb342702b892a
Admas <molla.admas@gmail.com>
2013-01-05 16:44:36 -0600

    it worked!

--- db/migrate/20120712001914_category.rb
+++
@@ -1,7 +0,0 @@
-class Category < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20120712002311_adddescriptiontocategory.rb
+++
@@ -1,7 +0,0 @@
-class Adddescriptiontocategory < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
