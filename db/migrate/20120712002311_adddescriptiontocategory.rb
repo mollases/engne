403d25d971b957d1c0b9031588c271709429934a
Krishna Halaharvi <halaharvi.krishna@gmail.com>
2013-01-05 17:11:55 -0600

    Revert "it worked!"
    This reverts commit 8b196701af1c46dbbca96a23d1abb342702b892a.

---
+++ db/migrate/20120712002311_adddescriptiontocategory.rb
@@ -0,0 +1,7 @@
+class Adddescriptiontocategory < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130105224209) do
+ActiveRecord::Schema.define(:version => 20120720093935) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -21,22 +21,14 @@ ActiveRecord::Schema.define(:version => 20130105224209) do
   end
 
   create_table "members", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "email"
+    t.string   "password_salt"
   end
-
-  add_index "members", ["email"], :name => "index_members_on_email", :unique => true
-  add_index "members", ["reset_password_token"], :name => "index_members_on_reset_password_token", :unique => true
 
   create_table "projects", :force => true do |t|
     t.string   "title"
