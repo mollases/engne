d42b55c1d1be35bffa2b4879567e303a09563eb4
Krishna Halaharvi <halaharvi.krishna@gmail.com>
2013-01-05 09:54:38 -0600

    update rails version

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.6'
+gem 'rails', '3.2.10'
 
 # Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
+#gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Engne::Application.config.secret_token = 'a9e4c316b0aa8896fde728f294710dd6bc7da590c0f2dd5eefb7bff6bfcd1918c4b85cbc6ff1b812792dc8c8dce3cf4ab762f289033f407996aeaf0b594a03ae'
+Engne::Application.config.secret_token = 'cd4a82f4b6e8ae96a349ae14099ece6a54505755957c8bacac61ca5560b45e2baf5a21366b86833e3429947f5d65f2c9923f8698ad834c843a4503d07905a5c3'
