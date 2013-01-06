981cbfd57086b78a99d8988f2e7a68ce498068ee
Admas <molla.admas@gmail.com>
2013-01-06 12:47:03 -0600

    finished sign in, sign out, and sign up

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,5 +7,6 @@ class Member < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  has_many :projects
   # attr_accessible :title, :body
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Engne::Application.routes.draw do
 
   devise_for :members
 
-  get "log_in" => "sessions#new", :as => "log_in"
-  get "log_out" => "sessions#destroy", :as => "log_out"
-  get "sign_up" => "members#new", :as => "sign_up"
+ # get "log_in" => "sessions#new", :as => "log_in"
+ # get "log_out" => "sessions#destroy", :as => "log_out"
+ # get "sign_up" => "members#new", :as => "sign_up"
   get "index" => "home#index"
   
+  get "categories/new" => "home#index"
+  get "/categories/:id/edit(.:format)" => "categories#show"
+  delete "/categories/:id" => "categories#show"
 
-  root :to => "home#index"
+  root :to => "home#index", :as => "index"
   
   resources :sessions
   
