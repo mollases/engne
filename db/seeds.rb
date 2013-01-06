b2c1d4f332bd50b3a4183b0d4c60fb15338edf70
Admas <molla.admas@gmail.com>
2013-01-06 15:25:12 -0600

    home page nearly complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.create([{title: 'Jewerly'},{title: 'Ceramic Work'}])
+Category.create([{title: 'Jewerly'},{title: 'Ceramic Work'},{title: 'Metal Working'},{title: 'Painting'}])
 #Members.create([{first_name: 'Admas'}, {last_name: 'Molla'}, {password_digest: ''}, {email: ''}])
 #Members.create([{first_name: 'Harvey'}, {last_name: 'Halaharvey'}, {password_digest: ''}, {email: ''}])
