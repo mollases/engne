52224e3cd8c52c49de32247ebd61472310d9e5d4
Admas <molla.admas@gmail.com>
2013-01-13 14:09:05 -0600

    created a partial for member information

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1.json
   def show
     @project = Project.find(params[:id])
+    @member = Member.find(@project.member_id)
 
     respond_to do |format|
       format.html # show.html.erb
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,9 @@
 class Project < ActiveRecord::Base
-  attr_accessible :title, :estimated_investment_needed, :funds_due_date, :estimated_completion_date, :member_id, :category_id, :number_of_shares, :why_description, :how_description, :make_public, :estimated_costs
+  attr_accessible :title, :estimated_investment_needed, :funds_due_date, :estimated_completion_date
+  attr_accessible :member_id, :category_id, :number_of_shares, :why_description, :how_description, :make_public, :estimated_costs
   validates :title, uniqueness: true
-  validates_presence_of 	:title, :estimated_investment_needed, :funds_due_date, :estimated_completion_date, :member_id, :category_id, :number_of_shares, :why_description, :how_description, :make_public, :estimated_costs
+  validates_presence_of 	:title, :estimated_investment_needed, :funds_due_date, :estimated_completion_date
+  validates_presence_of :member_id, :category_id, :number_of_shares, :why_description, :how_description, :estimated_costs
   belongs_to :category
   belongs_to :member
 end
