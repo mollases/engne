534d58a9bc8b0ec8720c0bea12fa5d9e2c26116e
Admas <molla.admas@gmail.com>
2013-01-14 08:06:33 -0600

    playing with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Engne::Application.routes.draw do
 
-  get "transact/donate" => "transact#donate", :path => "/projects/:id/donate"
-  get "transact/invest" => "transact#invest", :path => "/projects/:id/invest"
-  get "transact/buy"    => "transact#buy", :path => "/projects/:id/buy"
-  get "transact/sell"   => "transact#sell", :path => "/projects/:id/sell"
-
-  get "page/about" => "page#about", :path => "/about"
-  get "page/privacy" => "page#about", :privacy => "/privacy"
-  get "page/terms" => "page#terms", :path => "/terms"
-  get "page/help" => "page#help", :path => "/help"
+  # get "page/about" => "page#about", :path => "/about"
+  # get "page/privacy" => "page#about", :privacy => "/privacy"
+  # get "page/terms" => "page#terms", :path => "/terms"
+  # get "page/help" => "page#help", :path => "/help"
 
   devise_for :members
 
@@ -21,7 +16,14 @@ Engne::Application.routes.draw do
   root :to => "home#index", :as => "index"
   
   resources :categories
-  resources :projects
+  resources :projects do
+    member do
+      get 'donate' => 'transact#donate'
+      get 'invest' => 'transact#invest'
+      get 'sell' => 'transact#sell'
+      get 'buy' => 'transact#buy'
+    end
+  end
   resources :members, :only => [:show], :as => "member"
 
   end
