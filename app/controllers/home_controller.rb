7f78e57b37b5a7a0b43f7f29695c4b9cd32197a7
Admas <molla.admas@gmail.com>
2013-01-16 17:13:23 -0600

    partial stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def index
     @categories = Category.all
-    @projects		= Project.find_all_by_project_status(Project.PROJECT_STATUS[:in_progress])
+    @buyable		= Project.all
+    @investable = Project.all
+
+    # => .find_all_by_project_status(Project.PROJECT_STATUS[:in_progress])
     # @investable		= Project.find_all_by_project_status(Project.PROJECT_STATUS[0])
   end
 end
