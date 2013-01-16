2d2097d863a7a93c1271ec7b3ec917e9012904d3
Admas <molla.admas@gmail.com>
2013-01-15 23:11:32 -0600

    figured it out, on to the next challenge: accessing an array

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 class HomeController < ApplicationController
   def index
     @categories = Category.all
-    @buyable		= Project.find(1)
-
-    #Project.find_all_by_project_status(Project.PROJECT_STATUS[:open_for_sale])
+    @projects		= Project.find_all_by_project_status(Project.PROJECT_STATUS[:in_progress])
     # @investable		= Project.find_all_by_project_status(Project.PROJECT_STATUS[0])
   end
 end
