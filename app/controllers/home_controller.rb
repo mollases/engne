ab25796a3ed093732ca5df7f4d20c4cbfa534f21
Admas <molla.admas@gmail.com>
2013-01-16 21:24:53 -0600

    shabaz palaces

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @categories = Category.all
     @buyable		= Project.find_all_by_project_status(Project.PROJECT_STATUS[:in_progress])
-    @investable = Project.all
+    @investable = Project.find_all_by_project_status(Project.PROJECT_STATUS[:publicly_investable])
   end
 end
