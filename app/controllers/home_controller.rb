63a1bcf06914f61dcb427c96e045fd0a9356459f
Admas <molla.admas@gmail.com>
2013-01-16 18:30:37 -0600

    optimization, and fading css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,7 @@
 class HomeController < ApplicationController
   def index
     @categories = Category.all
-    @buyable		= Project.all
+    @buyable		= Project.find_all_by_project_status(Project.PROJECT_STATUS[:in_progress])
     @investable = Project.all
-
-    # => .find_all_by_project_status(Project.PROJECT_STATUS[:in_progress])
-    # @investable		= Project.find_all_by_project_status(Project.PROJECT_STATUS[0])
   end
 end
