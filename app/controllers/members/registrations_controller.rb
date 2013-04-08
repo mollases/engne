88187f6279a0ae6d3fe8035cc881950641fed341
krishna halaharvi <halaharvi.krishna@gmail.com>
2013-04-07 20:48:45 -0500

    upload Module working? shelf?

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -1,4 +1,9 @@
 class Members::RegistrationsController < Devise::RegistrationsController
 	def edit
 	end
+	def upload
+	
+		photo = Uploader.save(params[:member_photo])
+		render :text => "File has been uploaded successfully"
+	end
 end 
